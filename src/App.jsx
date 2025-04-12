ddd0165c242481dbf3324b5ba0e08c27f424fcd2
Pablo <araujopablo95@gmail.com>
2025-04-12 12:18:46 -0300

    Creacion componente Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,67 @@
+import Header from "./components/Header"
+import { useState } from "react"
+import Button from "./components/Button";
 
 function App() {
+  const [cantidad, setCantidad] = useState(10000);
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+  function handleChange(e){
+    setCantidad(e.target.value);
+  }
   
+  function handleClickDecremento(){
+    const valor = cantidad - STEP
+
+    if(valor < MIN){
+      alert('Cantidad no valida');
+      return;
+    }
+
+    setCantidad(valor);
+  }
+
+  function handleClickIncremento(){
+    const valor = cantidad + STEP
+
+    if(valor > MAX){
+      alert('Cantidad no valida');
+      return;
+    }
+
+    setCantidad(valor);
+  }
+
   return (
-    <h1 className="text-6xl font-extrabold">Hola mundo</h1>
+    <div className="my-20 max max-w-lg mx-auto bg-white shadow p-10">
+      <Header/>
+
+      <div className="flex justify-between my-6">
+        <Button
+          operador='-'
+          fn={handleClickDecremento}
+        />
+        <Button
+           operador='+'
+           fn={handleClickIncremento}
+        />
+      </div>
+      
+      <input 
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
+      />
+
+      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600"s>{cantidad}</p>
+      
+    </div>
   )
 }
 
